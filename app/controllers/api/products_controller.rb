f9e49187142a293286298f3f25b204f037dc270e
Isaiah Rappaport <irappap98@gmail.com>
2020-09-01 20:41:35 -0400

    add products to json for viewable to user

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def martin_guitar_action
+    @martin = Product.first
+    render "martin_guitar_action.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/martin_guitar" => "products#martin_guitar_action"
+  end
 end
