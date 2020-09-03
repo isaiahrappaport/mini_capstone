10089bc0e88da25a6a22f0171bf0d7d932179114
Isaiah Rappaport <irappap98@gmail.com>
2020-09-02 20:50:18 -0400

    add query parameter for single product display

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,10 +1,6 @@
 class Api::ProductsController < ApplicationController
-  def martin_guitar_action
-    @martin = Product.first
-    render "martin_guitar_action.json.jb"
-  end
-  def taylor_guitar_action
-  @taylor = Product.second
-  render "taylor_guitar.json.jb"
+  def single_product_action
+    @product = Product.find_by(id: params["product_id"])
+    render "single_product_action.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/martin_guitar" => "products#martin_guitar_action"
-    get "/taylor_guitar" => "products#taylor_guitar_action"
+    get "/product" => "products#single_product_action"
   end
 end
