adbdfd427c5f4a36518a809171ab321c2bb2beb4
Isaiah Rappaport <irappap98@gmail.com>
2020-09-08 19:44:59 -0400

    add validations for name, price, description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 2..500 }
+
   def friendly_updated_at
     updated_at.strftime("%D")
   end
