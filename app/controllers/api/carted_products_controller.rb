ad2cd8c7e72e33cdab9477b40428eeb77f4557f9
Isaiah Rappaport <irappap98@gmail.com>
2020-09-17 22:30:47 -0400

    add create and index method for carted_products controller

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -5,6 +5,18 @@ class Api::CartedProductsController < ApplicationController
     @carted_product = CartedProduct.new(
       product_id: params[:product_id],
       quantity: params[:quantity],
+      user_id: current_user.id,
+      status: "carted",
     )
+    if @carted_product.save
+      render "carted_products.json.jb"
+    else
+      render json: { message: @carted_product.errors.full_messages }
+    end
+  end
+
+  def index
+    @carted_products = current_user.carted_products
+    render "index.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
     get "/orders/:id" => "orders#show"
 
     post "/carted_products/" => "carted_products#create"
+    get "/carted_products" => "carted_products#index"
   end
 end
